93cd89f0a150d731a1dc6c7c4719c37bdc6e4d2c
mr_destructive <gormeet711@gmail.com>
2022-05-27 17:50:48 +0530

    fixed links from githubpages

--- plugins/feeds.py
+++ plugins/feeds.py
@@ -120,7 +120,7 @@ def create_card(post, template=None):
                 f"""
                 <li class='post'>
                 <img src="{post['image_url']}" class="cover-image" >
-                <a href="/techstructive-blog/{post['slug']}/">
+                <a href="/{post['slug']}/">
                    <h2 id="title"> {post['title']} </h2>
                    <span>{ post['date'].strftime('%d-%m-%Y')  }</span>
                 </a>
@@ -131,7 +131,7 @@ def create_card(post, template=None):
             return textwrap.dedent(
                 f"""
                 <li class='post'>
-                <a href="/techstructive-blog/{post['slug']}/">
+                <a href="/{post['slug']}/">
                    <h2 id="title"> {post['title']} </h2>
                 </a>
                 </li>
