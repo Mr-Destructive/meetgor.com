7a32fbf6dcb018e5025dccd94acee9e17c184a09
mr-destructive <gormeet711@gmail.com>
2025-12-13 22:19:11 +0530

    snow effects and other seasonal effects

--- static/sql-playground.js
+++ static/sql-playground.js
@@ -142,6 +142,9 @@ document.addEventListener('DOMContentLoaded', async () => {
                 lineNumbers: true,
                 viewportMargin: Infinity
             });
+            setTimeout(() => {
+                editor.refresh();
+            }, 10);
             editButton.innerHTML = '<svg xmlns="http://www.w3.org/2000/svg" width="16" height="16" viewBox="0 0 24 24" fill="none" stroke="currentColor" stroke-width="2" stroke-linecap="round" stroke-linejoin="round"><circle cx="12" cy="12" r="10"></circle><line x1="15" y1="9" x2="9" y2="15"></line><line x1="9" y1="9" x2="15" y2="15"></line></svg> Close';
             updateUITheme();
         };
